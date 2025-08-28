bb8f67dbd78d9fc98d14494295ade3357a458ec9
Joshua Ferugson <jferguson32@huskers.unl.edu>
2025-08-28 13:10:48 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Joshua Ferguson
+ *         jferguson32@huskers.unl.edu
+ * Date: 2025/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joshua Ferguson\n");
+
+  return 0;
+}
+
